8843c5312e8c3fd32be49b6f8a86969fa875f415
Ramkishore Ramkumar <rram0029@student.monash.edu>
2026-05-13 14:32:37 +1000

    Added area chart for per capita emissions

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,5 +1,9 @@
 var spec1 = "js/map.vg.json";
+var spec6 = "js/vis6_chart.vg.json";
 
 vegaEmbed('#vis1', spec1, { "config": { "background": "transparent" } }).then(function(result) {
     // Access the Vega view instance as result.view
+}).catch(console.error);
+
+vegaEmbed('#vis6', spec6, { "config": { "background": "transparent" } }).then(function(result) {
 }).catch(console.error);
